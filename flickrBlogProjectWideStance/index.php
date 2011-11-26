1cd8e8317ef95476bf80cc16e3768141915c0f90
Eric Gelinas <eric@standardpixel.com>
2011-11-26 06:26:39 +0000

    add wide post content

--- flickrBlogProjectWideStance/index.php
+++ flickrBlogProjectWideStance/index.php
@@ -17,8 +17,97 @@
 </head>
 <body>
 	<div id="main_container">
-		<header></header>
+		<header>
+			<h1>Photo Blog</h1>
+			<a href="#">Flickr.com</a>
+			<a href="#">Code Blog</a>
+			<a href="#">Explore</a>
+			<a href="#">Community Events <div style="color:#fff;background-color:#FF0084;padding:3px;display:inline-block;-webkit-border-radius:999px;font-size:12px;">30</div></a>
+			
+		</header>
 		<div id="content_container">
+			<div class="entry-date"><abbr class="published" title="2011-11-24">November 24, 2011</abbr></div>
+			<h2 class="entry-title"><a href="http://blog.flickr.net/en/2011/11/24/delicious-thanksgiving-everyone/" title="Permalink to Delicious Thanksgiving&nbsp;everyone!" rel="bookmark">Delicious Thanksgiving&nbsp;everyone!</a></h2>
+			<div class="post-content">
+				<div id="post-31101" class="entry">
+
+								<div class="entry-content">
+				<p class="sixforty"><a href="http://www.flickr.com/photos/prescod/1538075613/" title="Brined, High-Roast Turkey by Lily White, on Flickr">
+					<img src="http://farm3.staticflickr.com/2321/1538075613_c9ff98dfaf_b.jpg" alt="Brined, High-Roast Turkey" border="0" />
+				</a></p>
+
+				<p class="sixforty"><a href="http://www.flickr.com/photos/hanoirockz/3430061378/" title="wild turkey on the table YEAH! by Phil die Orangenmandarine, on Flickr">
+					<img src="http://farm4.static.flickr.com/3616/3430061378_d6fa0dc21d_b.jpg" width="504px" height="337px" alt="wild turkey on the table YEAH!" border="0" />
+				</a>&nbsp;&nbsp;
+				<a href="http://www.flickr.com/photos/jdavid/304997512/" title="Turkey! by j david, on Flickr">
+					<img src="http://farm1.static.flickr.com/101/304997512_4560b298f8_b.jpg" width="504px" height="337px" alt="Turkey!" border="0" />
+				</a></p>
+				
+				<p class="sixforty"><a href="http://www.flickr.com/photos/parkerman/2269328944/" title="Oven-baked Sweet Potato Fries by Sashertootie, on Flickr">
+					<img src="http://farm3.static.flickr.com/2019/2269328944_170329ced9_b.jpg" alt="Oven-baked Sweet Potato Fries" border="0" />
+				</a></p>
+
+				<p class="sixforty"><a href="http://www.flickr.com/photos/erincooks/2321722273/" title="Baked Sweet Potato Latkes by erincooks, on Flickr">
+					<img src="http://farm4.static.flickr.com/3006/2321722273_9f4ac8631b_b.jpg" width="504px" height="337px" alt="Baked Sweet Potato Latkes" border="0" />
+				</a>
+				&nbsp;&nbsp;
+				<a href="http://www.flickr.com/photos/cookincanuck/3821701047/" title="Grilled Sweet Potatoes with Pancetta &amp; Sage Vinaigrette by CookinCanuck, on Flickr">
+					<img src="http://farm3.static.flickr.com/2514/3821701047_465c6bb3ce_b.jpg" width="504px" height="337px" alt="Grilled Sweet Potatoes with Pancetta &amp; Sage Vinaigrette" border="0" />
+				</a></p>
+
+				<p class="sixforty"><a href="http://www.flickr.com/photos/ayngelina/4215431125/" title="Cranberry Sauce by ayngelina, on Flickr">
+					<img src="http://farm3.static.flickr.com/2710/4215431125_9a60e590ac_b.jpg" alt="Cranberry Sauce" border="0" />
+				</a></p>
+
+				<p class="sixforty"><a href="http://www.flickr.com/photos/kendra-jane/2052985559/" title="My favorite cranberry sauce by kendra-jane, on Flickr">
+					<img src="http://farm3.static.flickr.com/2046/2052985559_f2eb5ac971_b.jpg" width="504px" height="337px" alt="My favorite cranberry sauce" border="0" />
+				</a>
+				&nbsp;&nbsp;
+				<a href="http://www.flickr.com/photos/cookincanuck/4114219808/" title="Maple &amp; Brown Sugar Cranberry Sauce Recipe by CookinCanuck, on Flickr">
+					<img src="http://farm3.static.flickr.com/2574/4114219808_1bfc4f1a5e_b.jpg" width="504px" height="337px" alt="Maple &amp; Brown Sugar Cranberry Sauce Recipe" border="0" />
+				</a></p>
+
+				<p class="sixforty"><a href="http://www.flickr.com/photos/layladishman/5713436293/" title="Maple Pumpkin Pie (Dairy free) by Layla Dishman, on Flickr">
+					<img src="http://farm4.static.flickr.com/3253/5713436293_2761ecba22_b.jpg" alt="Maple Pumpkin Pie (Dairy free)" border="0" />
+				</a></p>
+				<p class="sixforty"><a href="http://www.flickr.com/photos/djwtwo/6367049055/" title="Pumpkin Pie Soufflé by djwtwo, on Flickr">
+					<img src="http://farm7.static.flickr.com/6051/6367049055_be94ff9338_z.jpg" width="504px" height="337px" alt="Pumpkin Pie Soufflé" border="0" />
+				</a>
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+				<a href="http://www.flickr.com/photos/ohiomom/2059088315/" title="Double Layer Pumpkin Pie by AprilnotMay, on Flickr">
+					<img src="http://farm3.static.flickr.com/2408/2059088315_e73194ed7d_z.jpg" width="204" height="240" alt="Double Layer Pumpkin Pie" border="0" />
+				</a>
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+				<a href="http://www.flickr.com/photos/spacekadet/3543141794/" title="Mini Pumpkin Pie by monica.shaw, on Flickr">
+					<img src="http://farm3.static.flickr.com/2422/3543141794_08640751cb_z.jpg" width="211" height="240" alt="Mini Pumpkin Pie" border="0" />
+				</a></p>
+
+				<p class="sixforty"><a href="http://www.flickr.com/photos/queenofthemoodswingset/4108060562/" title="Homemade pumpkin pie with homemade whipped cream by queenofthemoodswingset2, on Flickr"><img src="http://farm3.static.flickr.com/2525/4108060562_f9c1c4e8f8_b.jpg" alt="Homemade pumpkin pie with homemade whipped cream" border="0" /></a></p>
+
+
+			    <div class="entry-languages">
+			    	    				    			</div>
+				<p class="sixforty">A happy Thanksgiving to all our members in the USA, and around the world. If your menu is not yet complete, or if you are not celebrating this holiday, but nevertheless want to delve into the spirit of it, here are some mouth-watering photos from the <a href="http://www.flickr.com/groups/recipes/">Recipes to Share</a> <a href="http://www.flickr.com/groups/recipes/pool/">pool</a>. Every one of them come with detailed instructions on how to prepare the dish.</p>
+				<p class="sixforty">If you want to share your own, very special <a href="http://www.flickr.com/search/groups/?w=34955636712%40N01&amp;m=pool&amp;q=turkey">Turkey</a>, <a href="http://www.flickr.com/search/groups/?m=pool&amp;w=34955636712%40N01&amp;q=sweet+potatoes">Sweet Potatoe</a>, <a href="http://www.flickr.com/search/groups/?m=pool&amp;w=34955636712%40N01&amp;q=cranberry+sauce">Cranberry Sauce</a>, or <a href="http://www.flickr.com/search/groups/?m=pool&amp;w=34955636712%40N01&amp;q=pumpkin+pie">Pumpkin Pie</a> recipe, why not take a picture add the recipe to the description and contribute it to the group for everyone to try?  Enjoy a delicious Thanksgiving!</p>
+				<p class="sixforty">Photos from <a href="http://www.flickr.com/photos/prescod/">Lily White</a>, <a href="http://www.flickr.com/photos/hanoirockz/">Phil die Orangenmandarine</a>, <a href="http://www.flickr.com/photos/jdavid/">j david</a>, <a href="http://www.flickr.com/photos/parkerman/">Sashertootie</a>, <a href="http://www.flickr.com/photos/erincooks/">erincooks</a>, <a href="http://www.flickr.com/photos/cookincanuck/">CookinCanuck</a>, <a href="http://www.flickr.com/photos/layladishman/">Layla Dishman</a>, <a href="http://www.flickr.com/photos/djwtwo/">djwtwo</a>, <a href="http://www.flickr.com/photos/ohiomom/">AprilnotMay</a>, <a href="http://www.flickr.com/photos/spacekadet/">monica.shaw</a>, <a href="http://www.flickr.com/photos/queenofthemoodswingset/">queenofthemoodswingset2</a>, <a href="http://www.flickr.com/photos/kendra-jane/">kendra-jane</a>, and <a href="http://www.flickr.com/photos/ayngelina/">ayngelina</a>.</p>
+				<div class="sharedaddy"></div>
+								</div>
+
+								<div class="posted">
+				                	Posted by <b>Kay Kremerskothen</b><br />
+
+
+
+
+
+				                	<a href="http://blog.flickr.net/en/2011/11/24/delicious-thanksgiving-everyone/">Permalink</a> 
+
+				                </div>
+
+							</div>
+			</div>
+			
+			<h2>More from Flickr Blog</h2>
 			<? foreach ($feed->get_items() as $item) { ?>
 
 				<?= writePost($item); ?>
